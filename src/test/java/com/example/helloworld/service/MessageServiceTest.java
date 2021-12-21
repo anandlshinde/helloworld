c595d5378167416b7aadb29ebe5e74d05e786914
Anand Shinde <anandshinde.22@gmail.com>
2021-12-21 17:42:20 +0530

    Thirtd commit error with logger

--- src/test/java/com/example/helloworld/service/MessageServiceTest.java
+++ src/test/java/com/example/helloworld/service/MessageServiceTest.java
@@ -2,6 +2,8 @@ package com.example.helloworld.service;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,9 +13,12 @@ public class MessageServiceTest {
     @InjectMocks
     private MessageService messageService;
 
+    private static Logger log= LoggerFactory.getLogger(MessageServiceTest.class);
+
     @Test
     public void getMessageTest(){
         //String expected_Result="Hello Spring Boot World";
+        log.info("Test method executing ...");
         String actual_result=messageService.getMessage();
         assertEquals(expected_Result,actual_result);
     }
