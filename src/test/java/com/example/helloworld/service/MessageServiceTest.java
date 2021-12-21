64cec34c862e42550466040c297377374e1d58b9
Anand Shinde <anandshinde.22@gmail.com>
2021-12-21 17:14:30 +0530

    Second Commit with error

--- src/test/java/com/example/helloworld/service/MessageServiceTest.java
+++ src/test/java/com/example/helloworld/service/MessageServiceTest.java
@@ -13,7 +13,7 @@ public class MessageServiceTest {
 
     @Test
     public void getMessageTest(){
-        String expected_Result="Hello Spring Boot World";
+        //String expected_Result="Hello Spring Boot World";
         String actual_result=messageService.getMessage();
         assertEquals(expected_Result,actual_result);
     }
